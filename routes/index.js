baaa987b8bf40f25261f7358f83f480c17d767db
shengyueqing_windows <shengyueqing@gmail.com>
2020-03-29 11:18:04 -0400

    fix exec

--- routes/index.js
+++ routes/index.js
@@ -14,21 +14,18 @@ router.post('/', (req, res) => {
     }
   })
 })
-const spawn = require('child_process').spawn;
+
+
 
 router.get('/', function(req, res) {
-  var ls = spawn('./log.sh'/*command*/, []/*args*/, {}/*options, [optional]*/);
-  ls.stdout.on('data', function (data) {
-    console.log('stdout: ' + data);
-  });
-
-  ls.stderr.on('data', function (data) {
-    console.log('stderr: ' + data);
-  });
-
-  ls.on('exit', function (code) {
-    console.log('child process exited with code ' + code);
-  });
+  const commands = './log.sh';
+  exec(commands, (err, out, code) => {
+    if (err) {
+        res.send(err);
+    }else {
+        res.send(out)
+    }
+  })
 });
 
 module.exports = router;
